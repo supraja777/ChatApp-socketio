e4631f25cd4d9186d369f8621b39208f2186a85a
supraja777 <ssupraja777@gmail.com>
2023-09-30 05:54:33 +0530

    first commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const {protect} = require("../middleware/authMiddleware")
+const {registerUser, authUser, allUsers} = require("../controllers/userController")
+
+const router = express.Router()
+
+router.route('/').post(registerUser).get(protect,allUsers)
+router.post('/login', authUser)
+
+
+module.exports = router
